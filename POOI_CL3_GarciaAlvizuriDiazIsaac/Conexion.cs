95551a45dda6fc11e764f023b1b013f37e9690dc
Isaac <isaac769@hotmail.com>
2022-10-23 22:56:09 -0500

    codigo de clase conexión terminado

--- POOI_CL3_GarciaAlvizuriDiazIsaac/Conexion.cs
+++ POOI_CL3_GarciaAlvizuriDiazIsaac/Conexion.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace POOI_CL3_GarciaAlvizuriDiazIsaac
 {
-    internal class Conexion
+    public class Conexion
     {
+        public SqlConnection getConecta()
+        {
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cn"].ConnectionString);
+            return cn;
+        }
     }
 }
