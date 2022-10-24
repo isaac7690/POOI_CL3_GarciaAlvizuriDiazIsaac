5f138853cfe26e7b4c892282b01d1437f78865d4
Isaac <isaac769@hotmail.com>
2022-10-23 23:44:49 -0500

    codigo clase Enfermero terminado

--- POOI_CL3_GarciaAlvizuriDiazIsaac/Enfermero.cs
+++ POOI_CL3_GarciaAlvizuriDiazIsaac/Enfermero.cs
@@ -8,5 +8,10 @@ namespace POOI_CL3_GarciaAlvizuriDiazIsaac
 {
     internal class Enfermero
     {
+        public int ide_enf { get; set; }
+        public string nom_enf { set; get; }
+        public string ape_enf { set; get; }
+        public string tel_enf { set; get; }
+        public int ide_esp { set; get; }
     }
 }
